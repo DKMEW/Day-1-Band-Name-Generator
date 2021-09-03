2153ed6ac3639c3f1b1b958892e99c1a1d7a2284
Darren Kai Mew <darrenk.mew@gmail.com>
2021-09-03 10:17:17 +0200

    Main.py
    This displays a basis for python. 
    Variables and print statements. 
    Something fun that takes less time than making a sandwich

---
+++ Main.py
@@ -0,0 +1,6 @@
+print("Band Name Generator !")
+print("there are a few things we need first")
+city=input("Where were you born ?\n")
+pet=input("What is the name of your pet \n")
+
+print("BAND NAME IS : "+city+pet)
